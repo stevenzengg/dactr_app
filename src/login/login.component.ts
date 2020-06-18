164a3bd16c8c1e6f3c56baf9c7b0d129e43108bc
Yajur21 <61120383+Yajur21@users.noreply.github.com>
2020-06-17 20:35:41 -0400

    Login Update 06/17/2020

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LoginComponent implements OnInit {
+    public isLoading: boolean = false;
     constructor() { }
 
     ngOnInit() { }
+
+    login()
+    {
+        this.isLoading = true;
+        console.log("Log in is tapped")
+    }
 }
