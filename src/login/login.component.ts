213b785008ff9a318bacde5396a74db374f1fbe0
Yajur21 <61120383+Yajur21@users.noreply.github.com>
2020-06-20 18:06:40 -0400

    (1) Tapping "Sign up now" will now bring up "Confirm Password" text field (2) Tapping "Sign up now" will also change button text to "Sign Up"

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -10,6 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
     public isLoading: boolean = false
     public isLoggingIn = true;
+    public username: string;
+    public password: string;
+
     constructor() { }
 
     ngOnInit() { }
