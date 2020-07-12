a5767e0af6b5077b510c9064b03c3f8e834519a1
Steven Zeng <zengstevenz@gmail.com>
2020-07-11 20:28:40 -0400

    implemented landing page structure

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -2,32 +2,16 @@
 
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from "nativescript-angular/common";
-import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
-import { NativeScriptUIListViewModule } from "nativescript-ui-listview/angular";
-import { NativeScriptUICalendarModule } from "nativescript-ui-calendar/angular";
-import { NativeScriptUIChartModule } from "nativescript-ui-chart/angular";
-import { NativeScriptUIDataFormModule } from "nativescript-ui-dataform/angular";
-import { NativeScriptUIAutoCompleteTextViewModule } from "nativescript-ui-autocomplete/angular";
-import { NativeScriptUIGaugeModule } from "nativescript-ui-gauge/angular";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
-import { NativeScriptModule } from '@nativescript/angular/nativescript.module';
 
 import { LoginRoutingModule } from "./login-routing.module";
 import { LoginComponent } from './login.component';
 
 @NgModule({
     imports: [
-        NativeScriptUISideDrawerModule,
-        NativeScriptUIListViewModule,
-        NativeScriptUICalendarModule,
-        NativeScriptUIChartModule,
-        NativeScriptUIDataFormModule,
-        NativeScriptUIAutoCompleteTextViewModule,
-        NativeScriptUIGaugeModule,
         NativeScriptCommonModule,
         LoginRoutingModule,
-        NativeScriptFormsModule,
-        NativeScriptModule
+        NativeScriptFormsModule
     ],
     exports: [LoginComponent],
     declarations: [LoginComponent],
