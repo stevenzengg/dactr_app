6c769b04d76f0205faeaeb3926dea9374d7e7d98
EmperorMareep <30425007+EmperorMareep@users.noreply.github.com>
2020-07-30 22:04:28 -0400

    Downloaded NLP Library

--- src/patient-landing/journal/journal.component.ts
+++ src/patient-landing/journal/journal.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
+
 @Component({
     selector: 'journal',
     templateUrl: 'journal.component.html'
@@ -10,12 +13,32 @@ import { Component, OnInit } from '@angular/core';
 // We would need to access Firebase Fire DataStore as well
 export class JournalComponent implements OnInit {
     public journal;
+    
 
     constructor() { }
 
-    printJournal()
+    async printJournal()
     {
-        console.log(this.journal);
+        console.log(this.journal);       
+      
+        // Instantiates a client
+        const client = new language.LanguageServiceClient();
+      
+        // The text to analyze
+        const text = 'Hello, world!';
+      
+        const document = {
+          content: text,
+          type: 'PLAIN_TEXT',
+        };
+      
+        // Detects the sentiment of the text
+        const [result] = await client.analyzeSentiment({document: document});
+        const sentiment = result.documentSentiment;
+      
+        console.log(`Text: ${text}`);
+        console.log(`Sentiment score: ${sentiment.score}`);
+        console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
     }
 
     ngOnInit() { }
