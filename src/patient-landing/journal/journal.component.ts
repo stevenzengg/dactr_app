73df408f258032b71852a00d48add87ea60d200d
Divyesh Johri <divyeshjohri@gmail.com>
2020-08-05 19:11:51 -0400

    Cloud functions implemented, sentiment function created

--- src/patient-landing/journal/journal.component.ts
+++ src/patient-landing/journal/journal.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { EnvironmentManagerService } from '../../services/environment-manager.service';
-import { SentimentService } from '../../services/sentiment.service'
-
-
 
 @Component({
     selector: 'journal',
-    providers: [EnvironmentManagerService, SentimentService],
     templateUrl: 'journal.component.html'    
 })
 
@@ -15,36 +10,15 @@ import { SentimentService } from '../../services/sentiment.service'
 // We would need to access Firebase Fire DataStore as well
 export class JournalComponent implements OnInit {
     public journal;
-    private document;
-    private environment;
-    private sentimentAnalysis;
     
     
-    constructor(private envService: EnvironmentManagerService,
-        private senService: SentimentService) {
-        this.environment = new EnvironmentManagerService();
-        this.sentimentAnalysis = new SentimentService(); 
-     }
+    constructor(){}
 
      
     printJournal()
     {
         console.log(this.journal);
-        
-        this.document = {
-            content: this.journal,
-            type: 1       // PLAIN_TEXT
-        };
-
-        //this.sentimentAnalysis(this.document);
     }
-
-    // Imports the Google Cloud client library
-    async quickstart() {
-        const language = require('@google-cloud/language');
-        const client = new language.LanguageServiceClient({projectId: "dactr-app-20200711", keyFilename: 'C:/Users/divye/Downloads/dactr-app-20200711-4297127355a7.json'}); //{projectId: "dactr-app-20200711", keyFilename: this.environment.getGoogleNLPKey()}
-    }
-    
 
     ngOnInit() { }
 }
