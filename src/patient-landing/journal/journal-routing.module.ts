7cd72d28782ef54b301795a4549253ed37697662
Steven Zeng <zengstevenz@gmail.com>
2020-08-09 15:54:11 -0400

    hopeful fix for routing?

--- src/patient-landing/journal/journal-routing.module.ts
+++ src/patient-landing/journal/journal-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
-import { Routes } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
 import { NativeScriptRouterModule } from "@nativescript/angular/router";
 
 import { JournalComponent } from "./journal.component";
-import { FeedbackComponent } from "./feedback/feedback.component"
+import { FeedbackModule } from "./feedback/feedback.component"
 
 const routes: Routes = [
     { path: "journal", component: JournalComponent },
-    { path: "feedback", component: FeedbackComponent }
+    { path: "feedback", component: FeedbackModule }
 ];
 
 @NgModule({
