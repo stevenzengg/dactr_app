8685e6f2b6b4a45da8bf7cef3546149d99f81f52
Yajur21 <61120383+Yajur21@users.noreply.github.com>
2020-08-09 20:38:55 -0400

    Added ngModel back to input fields

--- src/clinician-landing/clinician-landing.component.ts
+++ src/clinician-landing/clinician-landing.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+
+const firebase = require("nativescript-plugin-firebase/app");
+
+firebase.initializeApp({});
+
+const userCollection = firebase.firestore().collection("user_database");
 
 @Component({
     selector: 'clinician-landing',
@@ -8,5 +15,23 @@ import { Component, OnInit } from '@angular/core';
 export class ClinicianLandingComponent implements OnInit {
     constructor() { }
 
+    private patientList = new Array<String>();
+
     ngOnInit() { }
+
+    setPatientList()
+    {
+        userCollection.get({source: "server"}).then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+
+
+                /*this.journal = doc.data().journal;
+                this.journalEntry.journal = this.journal;
+                */
+                //This pushes journal entry into journal log array
+                this.patientList.push(doc.data().firstName);
+            });
+        });
+    }
 }
