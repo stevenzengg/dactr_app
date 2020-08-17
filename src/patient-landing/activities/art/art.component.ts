5933a9940ebe60cf42da33fbdf96b9def5b2eb1c
TJ Schaeffer <tjs684@psu.edu>
2020-08-16 21:53:15 -0400

    Adding component files for html docs

---
+++ src/patient-landing/activities/art/art.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import{ getPlacesService } from "../../../services/getPlacesAPI.service"
+import{ getLocationService } from "../../../services/getLocation.service"
+
+@Component({
+    selector: 'art-component',
+    providers: [getPlacesService, getLocationService]
+    templateUrl: 'art.component.html'
+})
+
+export class NameComponent implements OnInit {
+    constructor(
+        private places: getPlacesService, 
+        private loc: getLocationService) { 
+
+    }
+
+    private getSearch(keyword) {
+        this.loc.getLocation().then(location => {
+            this.places.getPlacesFunct(location[0], location[1], keyword).then(results => {
+            
+
+            }).catch(e => console.log(e))
+        }).catch(e => console.log())
+        
+
+    }
+
+    ngOnInit() { }
+}
