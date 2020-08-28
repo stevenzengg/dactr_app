3da2f85610f994bc299f2ec310938acd45baa852
Yajur21 <61120383+Yajur21@users.noreply.github.com>
2020-08-27 21:28:20 -0400

    Clinician Framework

--- src/clinician-landing/clinician-landing-routing.module.ts
+++ src/clinician-landing/clinician-landing-routing.module.ts
@@ -4,11 +4,13 @@ import { NativeScriptRouterModule } from "@nativescript/angular/router";
 
 import { ClinicianLandingComponent } from "./clinician-landing.component";
 import { ClinicianLoginComponent } from "../clinician-login/clinician-login.component";
+import { ClinicianProfileComponent} from './clinician-profile/clinician-profile.component'
 import {AuthGuard} from "../auth-guard.service";
 
 const routes: Routes = [
     { path: "clinician-landing", component: ClinicianLandingComponent, canActivate: [AuthGuard]},
-    { path: "clinician-login", component: ClinicianLoginComponent}
+    { path: "clinician-login", component: ClinicianLoginComponent},
+    { path: "clinician-profile", component: ClinicianProfileComponent}
 ];
 
 @NgModule({
